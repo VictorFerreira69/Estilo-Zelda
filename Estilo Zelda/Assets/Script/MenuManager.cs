5f013584e833983867503701f83e1db4842fd613
VictorFerreira69 <victor13.09.08ferreira@gmail.com>
2025-03-13 13:36:01 -0300

    Fazendo codigo menu

---
+++ Estilo Zelda/Assets/Script/MenuManager.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    [SerializeField] private string NomeDaFase;
+    [SerializeField] private GameObject painelOpçao;
+    [SerializeField] private GameObject painelMenuPrincipal;
+    [SerializeField] private GameObject painelCredito;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void Jogar()
+    {
+        SceneManager.LoadScene(NomeDaFase);
+        Time.timeScale = 1f;
+
+    }
+    public void AbrirOpçao()
+    {
+        painelOpçao.SetActive(true);
+        painelMenuPrincipal.SetActive(false);
+    }
+    public void FecharOpçao()
+    {
+        painelOpçao.SetActive(false);
+        painelMenuPrincipal.SetActive(true);
+    }
+    public void AbrirCredito()
+    {
+      painelCredito.SetActive(true);
+        painelOpçao.SetActive(false);
+        painelMenuPrincipal.SetActive(false);
+    }
+
+    public void Sair()
+    {
+        Application.Quit();
+    }
+}
