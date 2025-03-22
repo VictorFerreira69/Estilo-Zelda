c4f4be3586a1a42565d134c8e2f7454c6a4558e3
VictorFerreira69 <victor13.09.08ferreira@gmail.com>
2025-03-22 16:59:44 -0300

    Colocando animaçao do player
    coloquei todas as animaçoes que a gente vai usar ja no player

--- Estilo Zelda/Assets/Script/PlayerMove.cs
+++ Estilo Zelda/Assets/Script/PlayerMove.cs
@@ -5,27 +5,54 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerMove : MonoBehaviour
 {
-
     Rigidbody2D rb;
     float horizontal, vertical;
     PlayerStatus status;
+    Animator animator;
 
-    // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         status = GetComponent<PlayerStatus>();
+        animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame      
     void Update()
     {
         horizontal = Input.GetAxisRaw("Horizontal");
         vertical = Input.GetAxisRaw("Vertical");
+          if(horizontal < 0 && transform.localScale.x > 0)
+          {
+                transform.localScale = new Vector2(transform.localScale.x * -1,transform.localScale.y);
+          }
+          else if(horizontal > 0 && transform.localScale.x < 0)
+          {
+              transform.localScale = new Vector2(transform.localScale.x * -1,transform.localScale.y);
+          }
+      
+        bool isWalking = horizontal != 0 || vertical != 0;
+        animator.SetBool("isWalking", isWalking);
+
+      
+        if (Input.GetButtonDown("Fire1")) 
+        {
+            animator.SetTrigger("Attack");
+        }
     }
+
     private void FixedUpdate()
     {
         rb.velocity = new Vector2(horizontal, vertical) * status.Speed;
     }
-}
 
+    public void TakeHit()
+    {
+        animator.SetTrigger("Hit");
+    }
+
+    public void Die()
+    {
+        animator.SetTrigger("Death");
+        rb.velocity = Vector2.zero; 
+    }
+}
