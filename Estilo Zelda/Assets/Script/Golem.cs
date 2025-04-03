ca1913d459f0213daa3b2af4f08d5e3c4ce85310
VictorFerreira69 <victor13.09.08ferreira@gmail.com>
2025-04-02 21:53:16 -0300

    Fazendo a mecanica do golem

--- Estilo Zelda/Assets/Script/Golem.cs
+++ Estilo Zelda/Assets/Script/Golem.cs
@@ -4,20 +4,90 @@ using UnityEngine;
 
 public class Golem : CharactesStatus
 {
+    [SerializeField] private float detectionRange;
+    [SerializeField] private float attackCooldown;
+    [SerializeField] private GameObject rockPrefab;
+    
+    private Transform player;
+    private bool isAttacking = false;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player")?.transform;
+    }
+
     protected override void Teste()
     {
         throw new System.NotImplementedException();
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
+        if (player == null) return;
+
+        float distanceToPlayer = Vector2.Distance(transform.position, player.position);
+
+        if (distanceToPlayer <= detectionRange && !isAttacking)
+        {
+            StartCoroutine(Attack());
+        }
+    }
+
+    private IEnumerator Attack()
+    {
+        isAttacking = true;
+        animator.SetTrigger("ataque");
+        yield return new WaitForSeconds(0.5f); 
         
+        GameObject rock = SummonRock();
+        
+        if (rock != null)
+        {
+            yield return new WaitForSeconds(1f); 
+            DealDamage(rock);
+            Destroy(rock); 
+        }
+        
+        yield return new WaitForSeconds(attackCooldown);
+        isAttacking = false;
+    }
+
+    private GameObject SummonRock()
+    {
+        if (player == null) return null;
+        
+        GameObject rock = Instantiate(rockPrefab, player.position, Quaternion.identity);
+        
+        Animator rockAnimator = rock.GetComponent<Animator>();
+        if (rockAnimator != null)
+        {
+            rockAnimator.SetTrigger("spawn");
+            StartCoroutine(DestroyAfterAnimation(rock, rockAnimator));
+        }
+        
+        return rock;
+    }
+
+    private IEnumerator DestroyAfterAnimation(GameObject rock, Animator animator)
+    {
+        yield return new WaitForSeconds(animator.GetCurrentAnimatorStateInfo(0).length);
+        Destroy(rock);
+    }
+
+    private void DealDamage(GameObject rock)
+    {
+        if (player == null || rock == null) return;
+        
+        float distanceToPlayer = Vector2.Distance(rock.transform.position, player.position);
+        if (distanceToPlayer < 1f)
+        {
+            player.GetComponent<IDamageable>()?.TakeDamage(25);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, detectionRange);
     }
 }
